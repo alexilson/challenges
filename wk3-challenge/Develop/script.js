e3db30333c3fab9a8ec68eed889d37b5d11fe5b8
Alex <alex.ilson@outlook.com>
2023-09-28 23:13:19 -0700

    changed css to dark mode, tested out some alerts

--- wk3-challenge/Develop/script.js
+++ wk3-challenge/Develop/script.js
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// window.alert("Enter Length Of Passowrd")
+// var pwLength = prompt("Enter the length of password:")
+// window.alert("You have entered: " + pwLength)
+// window.alert("Do you want to include special characters?")
+// window.alert("What types of characters do you want to include?")
+// // Types: lowercase, uppercase, numeric, and/or special characters
+// window.alert("You have chosen: ")
+// window.alert("Here comes the password!!")
+// window.alert("password")
