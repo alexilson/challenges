c25c202566abf79c05f54cd5a47e13abc0b54643
Alex <alex.ilson@outlook.com>
2023-09-29 22:27:27 -0700

    Changed the object to use a nested object for the options instead of an array, and created the pwCreation algorithm to choose a random character out of a defined set of characters

--- wk3-challenge/Develop/script.js
+++ wk3-challenge/Develop/script.js
@@ -15,29 +15,60 @@ generateBtn.addEventListener("click", writePassword);
 
 //My code
 
+choiceSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+
 // declare function to get user input and generate pw, used in writePassword function
 function generatePassword() {
 
+  builtPw = "";
+  
   // create object with properties that will prompt for criteria and store it
   var pwCriteria = {
 
     // These are the choices the user will make
-    options: ["Lowercase", "Uppercase", "Numeric", "Special"], 
+    options: {
+      0: {
+        name: "Lowercase",
+        selected: "n",
+        set: "abcdefghijklmnopqrstuvwxyz"
+      },
+      1: {
+        name: "Uppercase",
+        selected: "n",
+        set: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      },
+      2: {
+        name: "Numeric",
+        selected: "n",
+        set: "0123456789"
+      },
+      3: {
+        name: "Special",
+        selected: "n",
+        set: "!@#$%^&*"
+      }
+    },
 
     // Ask for length
     pwLength: prompt("How long would you like the password to be? Enter a number between 8 and 128."),
     
     // ask for each of the options and store it in this object using the option name as the key
     pwSpecialChars() {
-      for (var i = 1; i < this.options.length; i++) {
-        this[this.options[i]] = prompt("Enter Y or N if you would like to include " + this.options[i] + " characters.");
+      for (var i = 1; i < Object.keys(this.options).length; i++) {
+        this[this.options[i].name] = prompt("Enter Y or N if you would like to include " + this.options[i].name + " characters.");
+      }
+    },
+
+    pwCreation() {
+      for (var i = 1; i < this.pwLength; i++) {
+        builtPw += choiceSet[(Math.floor((Math.random() * choiceSet.length)))]
       }
     }
   };
   
   // Run the method to ask for the choices
   pwCriteria.pwSpecialChars();
+  pwCriteria.pwCreation();
 
-  // Return the whole object i guess?
-  return pwCriteria;
+  return builtPw;
 }
